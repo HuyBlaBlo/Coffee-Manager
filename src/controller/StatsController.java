a292f86c9a674b582309b64afeb35ac599dd5f8b
Dao Gia Huy <huydgh04@gmail.com>
2026-04-30 12:12:56 +0700

    update file stats

---
+++ src/controller/StatsController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javax.swing.JPanel;
+
+public class StatsController extends JPanel{
+
+	public StatsController() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+}
--- src/ui/Stats.java
+++ src/ui/Stats.java
@@ -1,7 +1,17 @@
 package ui;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
 import javax.swing.JPanel;
 
 public class Stats extends JPanel{
+	public Stats() {
+		init();
+	}
 
+	private void init() {
+		setLayout(new BorderLayout());
+		setBackground(Color.decode(""));
+	}
 }
